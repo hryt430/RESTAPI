2fcbb34ae6c310100487052884f22c18669f23f6
Tony Hawkings <tonyhawkingsexample@example.com>
2025-04-06 18:39:42 +0900

    implement database interface

---
+++ api/internal/interfaces/database/user_handler.go
@@ -0,0 +1,100 @@
+package database
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hryt430/RESTAPI/api/internal/domain/entity"
+)
+
+type UserServiceRepository struct {
+	SqlHandler
+}
+
+func (repository *UserServiceRepository) FindUser() (users []*entity.User, err error) {
+	rows, err := repository.Query("SELECT id, first_name, last_name FROM users")
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var username string
+		var password string
+		if err := rows.Scan(&id, &username, &password); err != nil {
+			continue
+		}
+		user := entity.User{
+			ID:       id,
+			Username: username,
+			Password: password,
+		}
+		users = append(users, &user)
+	}
+	return
+}
+
+func (repository *UserServiceRepository) FindUserById(identifier int) (user *entity.User, err error) {
+	row, err := repository.Query("SELECT id, username, password FROM users WHERE id = ?", identifier)
+	if err != nil {
+		return
+	}
+	defer row.Close()
+
+	if row.Next() {
+		var id int
+		var username string
+		var password string
+
+		if err = row.Scan(&id, &username, &password); err != nil {
+			return
+		}
+
+		user = &entity.User{
+			ID:       id,
+			Username: username,
+			Password: password,
+		}
+		return
+	}
+
+	// データが見つからなかった場合
+	err = fmt.Errorf("user with id %d not found", identifier)
+	return
+}
+
+func (repository *UserServiceRepository) Save(u *entity.User) (id int, err error) {
+	result, err := repository.Execute(
+		"INSERT INTO users (username, password) VALUES (?, ?)", u.Username, u.Password,
+	)
+	if err != nil {
+		return
+	}
+	id64, err := result.LastInsertId()
+	if err != nil {
+		return
+	}
+	id = int(id64)
+	return
+}
+
+func (repository *UserServiceRepository) Edit(identifier int, u *entity.User) (id int, err error) {
+	return
+}
+
+func (repo *UserServiceRepository) Delete(id int) (err error) {
+	result, err := repo.Execute("DELETE FROM users WHERE id = ?", id)
+	if err != nil {
+		return
+	}
+
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return
+	}
+
+	if affected == 0 {
+		err = errors.New("no user found with given id")
+	}
+	return
+}
