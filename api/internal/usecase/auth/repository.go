b2ea9ced899931ac1458708f690c62110da84cbd
hryt <yuta430hr@icloud.com>
2025-04-14 23:28:55 +0900

    fixed usecase

--- api/internal/usecase/auth/repository.go
+++ api/internal/usecase/auth/repository.go
@@ -4,5 +4,5 @@ import "github.com/hryt430/RESTAPI/api/internal/domain/entity"
 
 type AuthServiceRepository interface {
 	GenerateToken(params *entity.User) (string, error)
-	Validate(token string) (*entity.User, error)
+	Validate(token string) (int, error)
 }
